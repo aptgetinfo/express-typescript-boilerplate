0bd66f7b8c3bd478f4fdbba56fef3998a84c91b5
aptgetinfo <deepak.2002.02.16@gmail.com>
2022-06-02 11:43:56 +0530

    express basic setup done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/', async (req, res) => {
+  res.json({ status: 'Online' });
+});
+
+const port = Number(process.env.PORT ?? 8080);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
